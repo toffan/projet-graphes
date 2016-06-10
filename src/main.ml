d65f56daf564b193411464919041166a21a1af31
Volodia Laniel <laniel.volodia@gmail.com>
2016-06-10 11:33:52 +0200

    Ajout de commentaires à la fonction tri_topologique et utilisation d'identifiants plus explicites.

--- src/main.ml
+++ src/main.ml
@@ -1,13 +1,14 @@
 open Dag.DAG
 
-let tri_topologique dag =
-    let list_vertex g =
-        fold_vertex (
-            fun v l ->
-                if (pred g v) = [] then v::l
+(* Calcule les sommets de g sans prédecesseurs. *)
+let list_root_vertex g = fold_vertex
+    (fun v l -> if (pred g v) = [] then v::l
                 else l
-        ) g []
-    and inclusion_aux z v y =
+    ) g [];;
+
+let tri_topologique dag =
+    let inclusion_aux z v y =
+        (* Liste des prédecesseurs de v dans dag. *)
         let lp = pred dag v in
         if (List.fold_right (fun v add ->
             add && (
@@ -17,14 +18,22 @@ let tri_topologique dag =
         ) lp true)
         then v::y
         else y
-    in let y = list_vertex dag and z = [] in
-    let rec tri_rec y z =
+
+    in let y = list_root_vertex dag and z = [] in
+
+    let rec tri_rec y z = (
         match y with
+        (* Si y est vide alors le calcul est terminé et le résultat est dans z. *)
         | [] -> z
-        | t::q ->
-                let zp = t::z
-                in let yp =
-                    let ls = succ dag t in
-                    List.fold_right (inclusion_aux zp) ls y
-                in tri_rec yp zp
+        | vi::q -> (
+                (* TODO: Numéroter vi. *)
+                (* zp : z à l'étape suivante.
+                 * yp : y à l'étape suivante. *)
+                let zp = vi::z in
+                let yp = (
+                    (* ls : liste des successeurs de vi (dans dag). *)
+                    let ls = succ dag vi in
+                    (* Calcul du nouvel ensemble Y. *)
+                    List.fold_right (inclusion_aux zp) ls y)
+                in tri_rec yp zp))
     in tri_rec y z;;
