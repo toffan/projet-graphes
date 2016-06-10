7b7926f98ab53f5bcbcf9b652df26b13f5ceef13
Volodia Laniel <laniel.volodia@gmail.com>
2016-06-10 15:36:34 +0200

    Marquage des noeuds tri_topologique (via Mark).

--- src/main.ml
+++ src/main.ml
@@ -21,12 +21,12 @@ let tri_topologique dag =
 
     in let y = list_root_vertex dag and z = [] in
 
-    let rec tri_rec y z = (
+    let rec tri_rec y z mark = (
         match y with
         (* Si y est vide alors le calcul est terminé et le résultat est dans z. *)
         | [] -> z
         | vi::q -> (
-                (* TODO: Numéroter vi. *)
+                Mark.set vi mark;
                 (* zp : z à l'étape suivante.
                  * yp : y à l'étape suivante. *)
                 let zp = vi::z in
@@ -35,5 +35,5 @@ let tri_topologique dag =
                     let ls = succ dag vi in
                     (* Calcul du nouvel ensemble Y. *)
                     List.fold_right (inclusion_aux zp) ls y)
-                in tri_rec yp zp))
-    in tri_rec y z;;
+                in tri_rec yp zp (mark+1)))
+    in tri_rec y z 1;;
