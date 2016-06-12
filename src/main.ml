d0c9fa58b7c9b35c647bcb4eb14bffe47c8f9274
Volodia Laniel <laniel.volodia@gmail.com>
2016-06-12 14:32:31 +0200

    MAJ de la documentation.

--- src/main.ml
+++ src/main.ml
@@ -1,11 +1,21 @@
 open Dag.DAG
 
-(* Calcule les sommets de g sans prédecesseurs. *)
+(* Entrées :
+   - un DAG dag
+ * Sorties :
+   - Liste des sommets de g sans prédecesseurs. *)
 let list_root_vertex g = fold_vertex
     (fun v l -> if (pred g v) = [] then l@[v]
                 else l
     ) g [];;
 
+
+(* Entrées:
+   - un DAG
+ * Sorties:
+   - une liste des sommets du DAG ordonnées selon un tri topologique
+ *  Spécifications :
+   - vous implementerez l'algorithme 1 de l'enonce, en utilisant un format de file pour Y (section 1) *)
 let tri_topologique dag =
     let inclusion_aux z y vj = (
         (* Liste des prédecesseurs de vj dans dag. *)
@@ -43,16 +53,19 @@ let tri_topologique dag =
                 in tri_rec yp zp (mark+1)))
     in tri_rec y z 1;;
 
+
+(* trace d'execution
+   definie en Section 2 de l'enonce (voir Figure 2) *)
 type trace = (Dag.DAG.vertex list) list
+
 
 (* Entrées :
    - un DAG dag
    - un noeud v
    - une liste de noeuds l
-   Sorties :
+ * Sorties :
    - vrai si l contient au moins un prédecesseur de v (dans dag).
-   - faux sinon.
-   *)
+   - faux sinon. *)
 let depend dag v l =
     let vp = pred dag v in
     (* Calcul de l'intersection de l et vp. *)
@@ -60,6 +73,16 @@ let depend dag v l =
         res || ((List.find_all (fun x -> x = e) l) != [])
     ) vp false;;
 
+
+(* Entrées:
+   - un nombre entier de ressources r
+   - un DAG
+ * Sorties:
+   - une trace d'execution du DAG
+ *  Spécifications :
+   - le DAG est suppose non pondere
+   - pas de contrainte mémoire (section 3)
+   - vous n'utiliserez pas d'heuristique *)
 let ordonnanceur_sans_heuristique r dag =
     let l = tri_topologique dag in
     List.rev (List.fold_left (fun trace v ->
@@ -68,24 +91,31 @@ let ordonnanceur_sans_heuristique r dag =
         if ((List.length current) >= r || (depend dag v current)) then
             [v]::trace
         else
+        (* Sinon on ajoute v à l'étage courant.*)
             (current@[v])::(List.tl trace)
     ) [[]] l);;
 
+
 (* Entrées :
    - Une liste de noeuds triés d'un DAG.
-   Sorties :
+ * Sorties :
    - unit
-   Sémantique : Marque les noeuds du DAG dans l'ordre de la liste. *)
+ * Sémantique : Marque les noeuds du DAG dans l'ordre de la liste.
+   Utilisée pour restaurer le marquage des noeuds par tri topologique. *)
 let marque_tri l =
     let null =
     List.fold_left (fun mark v -> Mark.set v mark; (mark+1)) 1 l in ();;
 
+
 (* Entrées :
    - un DAG dag
-   Sorties :
+   - un booléen permettant de changer la manière de calculer le chemin critique :
+     -> use_sum : somme des chemins critiques des fils.
+     -> !use_sum : 1 + maximum des chemins critiques des fils.
+ * Sorties :
    - unit
    Sémantique :
-   - Marque les noeuds avec leur taille de chemin critique (taille du plus long chemin reliant le noeud à un puit). *)
+   - Marque les noeuds avec leur taille de chemin critique. *)
 let marque_chemin_critique dag use_sum =
     let l = list_root_vertex dag in
     let rec aux v = (
@@ -101,22 +131,27 @@ let marque_chemin_critique dag use_sum =
         in Mark.set v vdepth; vdepth)
     in List.iter (fun v -> let null = aux v in ()) l;;
 
-(* Fonction de tri par taille de chemin critique.
+
+(* Fonction de comparaison par taille de chemin critique.
+   Utilisée pour trier une liste de noeuds par taille de chemin critique.
  * Entrées :
-   - un DAG dag
-   Sorties :
-   - Liste de noeuds triés par ordre décroissant de taille de chemin critique (taille du plus long chemin reliant le noeud à un puit)
-   - Pour deux noeuds ayant la même taille de chemin critique, on trie par ordre topologique croissant. *)
+   - deux noeuds à comparer v1 et v2.
+ * Sorties :
+   - 0 si m1 et m2 ont même taille de chemin critique.
+   - entier positif si m2 a un chemin critique plus grand que m1.
+   - entier négatif si m1 a un chemin critique plus grand que m2.
+   *)
 let tri_chemin_critique v1 v2 =
     let m1 = Mark.get v1 and m2 = Mark.get v2 in
     compare m2 m1;;
+
 
 (* Entrées :
    - un DAG dag
    - un noeud v
    - une liste de noeuds l
-   Sorties :
-   - vrai si l contient au tous les prédecesseurs de v (dans dag).
+ * Sorties :
+   - vrai si l contient tous les prédecesseurs de v (dans dag).
    - faux sinon.
    *)
 let dependances_satisfaites dag v l =
@@ -126,9 +161,10 @@ let dependances_satisfaites dag v l =
         res && ((List.find_all (fun x -> x = e) l) != [])
     ) vp true;;
 
+
 (* Entrées :
    - une liste de noeuds step.
-   Sorties :
+ * Sorties :
    - La somme des mémoires requises par chaque noeud de step. *)
 let mem_sum step =
     List.fold_right (fun v m ->
@@ -139,8 +175,9 @@ let mem_sum step =
 (* Entrées:
    - un nombre entier de ressources r
    - memoire disponible m ou 0 si l'on se place dans le cas sans contraintes de mémoire.
+   - un booléen optimiser_memoire (utilise une heuristique adaptée à la gestion de la mémoire).
    - un DAG
-   Sorties:
+ * Sorties:
    - une trace d'execution du DAG
    *)
 let ordonnanceur_avec_heuristique_et_memoire r m optimiser_memoire dag =
@@ -188,12 +225,46 @@ let ordonnanceur_avec_heuristique_et_memoire r m optimiser_memoire dag =
         )
     );;
 
+
+(* Entrées:
+   - un nombre entier de ressources r
+   - un DAG
+ * Sorties:
+   - une trace d'execution du DAG
+ *  Spécifications :
+   - le DAG est suppose non pondere
+   - pas de contrainte mémoire (section 3)
+   - vous utiliserez une heuristique pour ameliorer la duree de la trace *)
 let ordonnanceur_avec_heuristique r dag =
     ordonnanceur_avec_heuristique_et_memoire r 0 false dag;;
 
+
+(* Entrées:
+   - un nombre entier de ressources r
+   - memoire disponible M
+   - un DAG
+ * Sorties:
+   - une trace d'execution du DAG
+ *  Spécifications :
+   - le DAG est suppose non pondere
+   - on suppose une contrainte mémoire (section 4)
+   - vous utiliserez la meme heuristique que le cas non-contraint
+   *)
 let ordonnanceur_contrainte_memoire r m dag =
     ordonnanceur_avec_heuristique_et_memoire r m false dag;;
 
+
+(* Entrées:
+   - un nombre entier de ressources r
+   - memoire disponible M
+   - un DAG
+ * Sorties:
+   - une trace d'execution du DAG
+ *  Spécifications :
+   - le DAG est suppose non pondere
+   - on suppose une contrainte mémoire (section 4)
+   - vous utiliserez une heuristique specifique au cas contraint
+   *)
 let ordonnanceur_contrainte_memoire_bonus r m dag =
     ordonnanceur_avec_heuristique_et_memoire r m true dag;;
 
